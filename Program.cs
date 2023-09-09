56fe4388c109b8102eebe4c66e35261fc95612d7
Riiccardods <riicardoljn@icloud.com>
2023-09-09 18:15:11 -0300

    Aula

--- Program.cs
+++ Program.cs
@@ -8,68 +8,25 @@ namespace aula1
         {
 
 
-            bool continuar = true;
+            bool continuar = false;
 
-            while (continuar)
+            if(continuar == true) 
             {
-
-
-                Console.WriteLine("Pressione Enter para gerar um número Aleatório");
-                Console.ReadLine();
-
-                Random random = new Random();
-                int numeroAlea = random.Next(1, 101);
-
-
-                Console.WriteLine($"O numero Aleatório é: {numeroAlea}");
-                Console.WriteLine();
-                Console.WriteLine();
-
-
-
-
-                bool respostaValida = false;           // Flag para controlar a validação da resposta
-
-                while (!respostaValida)             // Enquanto a resposta não for válida
-                {
-                    Console.WriteLine("Deseja gerar outro numero? s/n");
-                    Console.WriteLine();
-                    char resposta = Console.ReadKey().KeyChar;
-
-                    if (resposta == 's' || resposta == 'S')
-                    {
-                        respostaValida = true;            // Marca a resposta como válida e sai do loop de validação
-                        Console.Clear();
-                    }
-                    else if (resposta == 'n' || resposta == 'N')
-                    {
-                        continuar = false;
-                        respostaValida = true;          // Marca a resposta como válida e sai do loop de validação
-                    }
-                    else
-                    {
-                        Console.Clear();
-                        Console.WriteLine("Digite somente: s/n - S/N");
-                        // Não marca como válida, então o loop de validação vai continuar
-                    }
-                }
-
-
-
-
-
-
-
+                Console.WriteLine("o valor e Verdadeiro");
             }
 
+            else 
+            {
+                Console.WriteLine("Falso");
+            
+            }
+
+           
 
 
 
 
 
-            Console.Clear();
-            Console.WriteLine("Volte Sempre!!");
-            Console.ReadLine();
         }
 
 
