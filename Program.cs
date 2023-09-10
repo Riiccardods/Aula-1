51de133046dba6c7a53f4072c45bba63e61d340b
Riiccardods <riicardoljn@icloud.com>
2023-09-09 21:35:05 -0300

    Aula

--- Program.cs
+++ Program.cs
@@ -8,25 +8,6 @@ namespace aula1
         {
 
 
-            bool continuar = true;
-            int contador = 0;
-
-            if (continuar == true) 
-            {
-                Console.WriteLine("o valor e Verdadeiro");
-                contador =+2;
-                Console.WriteLine(contador);
-            }
-
-            else 
-            {
-                Console.WriteLine("Falso");
-                continuar = true;
-            
-            }
-
-            Console.WriteLine($"agora mudou é: {continuar} ");
-
 
 
 
