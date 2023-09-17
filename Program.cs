931ee2b6b5d5b65b92bc0842dc3caaebb17a0fe6
Riiccardods <riicardoljn@icloud.com>
2023-09-17 09:21:41 -0300

    A

--- Program.cs
+++ Program.cs
@@ -6,21 +6,10 @@ namespace aula1
     {
         static void Main(string[] args)
         {
-            Random random = new Random();
-            int numero = random.Next(1,3);
 
-            if(numero >= 2) 
-            {
-                Console.WriteLine($"Seu numero é: {numero}");
-            
-            }
 
-            else
-            {
-                Console.WriteLine("O numero nao é nehnum encotrado");
-            }
 
-            Console.ReadLine();
+
 
 
         }
