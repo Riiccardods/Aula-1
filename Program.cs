60595f160b9a0b939c5bf2210b9e7ed148ac2557
riicardods <riicardoljn@icloud.com>
2023-09-28 20:27:36 -0300

    a

--- Program.cs
+++ Program.cs
@@ -1,36 +1,65 @@
-﻿
-Random random = new Random();
-int numero = random.Next(0, 11);
+﻿using System;
 
-int usuario;
-Console.WriteLine("digite um numero");
-if (int.TryParse(Console.ReadLine(), out usuario))
+class Program
 {
-
-
-
-    if (numero == usuario)
+    static int GerarNumeroAleatorio()
     {
-        Console.WriteLine("Acertou");
+        Random random = new Random();
+        return random.Next(1, 101); // Gere um número aleatório entre 1 e 100
     }
-    else if (numero > usuario)
+
+    static bool VerificarPalpite(int palpite, int numeroAleatorio)
     {
-        Console.WriteLine($"O número é maior. {numero}");
+        if (palpite < numeroAleatorio)
+        {
+            Console.WriteLine("Tente um número maior.");
+            return false;
+        }
+        else if (palpite > numeroAleatorio)
+        {
+            Console.WriteLine("Tente um número menor.");
+            return false;
+        }
+        else
+        {
+            Console.WriteLine($"Parabéns! Você adivinhou o número {numeroAleatorio}.");
+            return true;
+        }
     }
-    else
+
+    static void Main()
     {
-        Console.WriteLine($"O número é menor {numero}");
+        int[] tentativas = new int[10]; // Vamos limitar o jogo a 10 tentativas
+        int numeroAleatorio = GerarNumeroAleatorio();
+        int tentativasFeitas = 0;
+
+        Console.WriteLine("Bem-vindo ao jogo de adivinhação!");
+        Console.WriteLine("Tente adivinhar o número entre 1 e 100.");
+
+        while (tentativasFeitas < tentativas.Length)
+        {
+            Console.Write("Digite sua suposição: ");
+            if (int.TryParse(Console.ReadLine(), out int palpite))
+            {
+                tentativas[tentativasFeitas] = palpite;
+                tentativasFeitas++;
+
+                if (VerificarPalpite(palpite, numeroAleatorio))
+                {
+                    break; // O jogo termina se o usuário acertar
+                }
+            }
+            else
+            {
+                Console.WriteLine("Por favor, insira um número válido.");
+            }
+        }
+
+        if (tentativasFeitas == tentativas.Length)
+        {
+            Console.WriteLine($"Você excedeu o número máximo de tentativas. O número era {numeroAleatorio}.");
+        }
+
+        Console.WriteLine("O jogo acabou. Obrigado por jogar!");
     }
 }
-
-
-
-else
-{
-
-    Console.WriteLine("Entrada inválida. Por favor, insira um número inteiro válido.");
-}
-Console.ReadLine();
-
-
-
