97e8b392a0ad1a01aefa68c87605669d6388b068
riicardods <riicardoljn@icloud.com>
2023-09-28 21:29:39 -0300

    a

--- Program.cs
+++ Program.cs
@@ -1,65 +1,68 @@
 ﻿using System;
 
-class Program
+namespace JogoAdivinhacao
 {
-    static int GerarNumeroAleatorio()
+    class Program
     {
-        Random random = new Random();
-        return random.Next(1, 101); // Gere um número aleatório entre 1 e 100
-    }
-
-    static bool VerificarPalpite(int palpite, int numeroAleatorio)
-    {
-        if (palpite < numeroAleatorio)
+        static int GerarNumeroAleatorio()
         {
-            Console.WriteLine("Tente um número maior.");
-            return false;
+            Random random = new Random();
+            return random.Next(1, 101); // Gere um número aleatório entre 1 e 100
         }
-        else if (palpite > numeroAleatorio)
-        {
-            Console.WriteLine("Tente um número menor.");
-            return false;
-        }
-        else
-        {
-            Console.WriteLine($"Parabéns! Você adivinhou o número {numeroAleatorio}.");
-            return true;
-        }
-    }
 
-    static void Main()
-    {
-        int[] tentativas = new int[10]; // Vamos limitar o jogo a 10 tentativas
-        int numeroAleatorio = GerarNumeroAleatorio();
-        int tentativasFeitas = 0;
-
-        Console.WriteLine("Bem-vindo ao jogo de adivinhação!");
-        Console.WriteLine("Tente adivinhar o número entre 1 e 100.");
-
-        while (tentativasFeitas < tentativas.Length)
+        static bool VerificarPalpite(int palpite, int numeroAleatorio)
         {
-            Console.Write("Digite sua suposição: ");
-            if (int.TryParse(Console.ReadLine(), out int palpite))
+            if (palpite < numeroAleatorio)
             {
-                tentativas[tentativasFeitas] = palpite;
-                tentativasFeitas++;
-
-                if (VerificarPalpite(palpite, numeroAleatorio))
-                {
-                    break; // O jogo termina se o usuário acertar
-                }
+                Console.WriteLine("Tente um número maior.");
+                return false;
+            }
+            else if (palpite > numeroAleatorio)
+            {
+                Console.WriteLine("Tente um número menor.");
+                return false;
             }
             else
             {
-                Console.WriteLine("Por favor, insira um número válido.");
+                Console.WriteLine($"Parabéns! Você adivinhou o número {numeroAleatorio}.");
+                return true;
             }
         }
 
-        if (tentativasFeitas == tentativas.Length)
+        static void Main()
         {
-            Console.WriteLine($"Você excedeu o número máximo de tentativas. O número era {numeroAleatorio}.");
-        }
+            int[] tentativas = new int[10]; // Vamos limitar o jogo a 10 tentativas
+            int numeroAleatorio = GerarNumeroAleatorio();
+            int tentativasFeitas = 0;
 
-        Console.WriteLine("O jogo acabou. Obrigado por jogar!");
+            Console.WriteLine("Bem-vindo ao jogo de adivinhação!");
+            Console.WriteLine("Tente adivinhar o número entre 1 e 100.");
+
+            while (tentativasFeitas < tentativas.Length)
+            {
+                Console.Write("Digite sua suposição: ");
+                if (int.TryParse(Console.ReadLine(), out int palpite))
+                {
+                    tentativas[tentativasFeitas] = palpite;
+                    tentativasFeitas++;
+
+                    if (VerificarPalpite(palpite, numeroAleatorio))
+                    {
+                        break; // O jogo termina se o usuário acertar
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Por favor, insira um número válido.");
+                }
+            }
+
+            if (tentativasFeitas == tentativas.Length)
+            {
+                Console.WriteLine($"Você excedeu o número máximo de tentativas. O número era {numeroAleatorio}.");
+            }
+
+            Console.WriteLine("O jogo acabou. Obrigado por jogar!");
+        }
     }
 }
