b044bf21ea5b6a5a7f8a0a53440fde257185b4ab
riicardods <riicardoljn@icloud.com>
2023-10-01 16:40:43 -0300

    Estudadno Foreach

--- Program.cs
+++ Program.cs
@@ -1,33 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
 
 class Program
 {
     static void Main()
     {
-        // Criando uma lista de números inteiros.
-        List<int> numeros = new List<int>();
+        // Array de strings
+        string[] palavras = { "Olá", "mundo", "isso", "é", "um", "exemplo" };
 
-        // Adicionando elementos à lista
-        numeros.Add(10);
-        numeros.Add(20);
-        numeros.Add(30);
-
-        // Iterando sobre a lista e imprimindo os elementos.
-        Console.WriteLine("Elementos na Lista:");
-        foreach (int numero in numeros)
+        // Usando foreach para percorrer o array
+        foreach (string palavra in palavras)
         {
-            Console.WriteLine(numero);
-        }
-
-        // Removendo um elemento da lista..
-        numeros.Remove(20);
-
-        // Imprimindo a lista após a remoção..
-        Console.WriteLine("\nElementos na Lista após Remoção:");
-        foreach (int numero in numeros)
-        {
-            Console.WriteLine(numero);
+            // Imprimindo cada palavra
+            Console.WriteLine(palavra);
         }
     }
 }
