37bb812b51f0c3b5e4febc999c04ec6be8cfe24a
riicardods <riicardoljn@icloud.com>
2023-10-02 18:57:39 -0300

    a

--- Program.cs
+++ Program.cs
@@ -4,13 +4,16 @@ class Program
 {
     static void Main()
     {
-        // Vamos usar um loop for para imprimir um nome 10 vezes.
+        // Solicitar ao usuário que insira o nome
+        Console.Write("Digite seu nome: ");
+        string nome = Console.ReadLine();
 
-        // A estrutura básica do for é: for (inicialização; condição; expressão de iteração)
-        for (int i = 0; i < 10; i++)
-        {
-            Console.WriteLine("SeuNomeAqui");
-        }
+        // Solicitar ao usuário que insira a idade
+        Console.Write("Digite sua idade: ");
+        int idade = Convert.ToInt32(Console.ReadLine());
+
+        // Exibir as informações inseridas pelo usuário
+        Console.WriteLine($"Seu nome é {nome} e você tem {idade} anos.");
 
         // Aguardar o usuário pressionar Enter antes de fechar o console
         Console.ReadLine();
