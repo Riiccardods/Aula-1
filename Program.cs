14c82ca71f2946353c147f00122e31fbd454c74b
riicardods <riicardoljn@icloud.com>
2023-10-11 18:37:16 -0300

    a

--- Program.cs
+++ Program.cs
@@ -1,28 +1,94 @@
 ﻿using System;
 using System.Collections.Generic;
-using aula1;
 
 class Program
 {
     static void Main()
     {
-        // Criar alguns livros.
-        Livro livro1 = new Livro("Aprendend C# OOP", "Autor 1");
-        Livro livro2 = new Livro("POO em Prátic", "Autor 2");
+        List<string> listaDeCompras = new List<string>();
 
-        // Criar alguns usuários.
-        Usuario usuario1 = new Usuario("João");
-        Usuario usuario2 = new Usuario("Maria");
+        while (true)
+        {
+            Console.WriteLine("=== Lista de Compras ===");
+            ExibirListaDeCompras(listaDeCompras);
 
-        // Exemplo de Uso
-        usuario1.EmprestarLivro(livro1);
-        usuario2.EmprestarLivro(livro1);  // Tentando emprestar o mesmo livro.
+            Console.WriteLine("\nOpções:");
+            Console.WriteLine("1. Adicionar item");
+            Console.WriteLine("2. Remover item");
+            Console.WriteLine("3. Limpar lista");
+            Console.WriteLine("4. Sair");
 
-        usuario2.EmprestarLivro(livro2);
-        usuario1.DevolverLivro(livro1);
+            Console.Write("Escolha uma opção: ");
+            string escolha = Console.ReadLine();
 
-        // Mostrar status dos livros
-        Console.WriteLine($"Status do Livro 1: Emprestad - {livro1.EstaEmprestado}");
-        Console.WriteLine($"Status do Livro 2: Emprestado - {livro2.EstaEmprestado}");
+            switch (escolha)
+            {
+                case "1":
+                    Console.Write("Digite o item a ser adicionado: ");
+                    string item = Console.ReadLine();
+                    AdicionarItem(listaDeCompras, item);
+                    break;
+
+                case "2":
+                    Console.Write("Digite o item a ser removido: ");
+                    string itemRemover = Console.ReadLine();
+                    RemoverItem(listaDeCompras, itemRemover);
+                    break;
+
+                case "3":
+                    LimparLista(listaDeCompras);
+                    break;
+
+                case "4":
+                    Console.WriteLine("Saindo do programa. Até mais!");
+                    return;
+
+                default:
+                    Console.WriteLine("Opção inválida. Tente novamente.");
+                    break;
+            }
+
+            Console.Clear(); // Limpa o console para exibir o menu novamente
+        }
+    }
+
+    static void ExibirListaDeCompras(List<string> lista)
+    {
+        if (lista.Count == 0)
+        {
+            Console.WriteLine("A lista de compras está vazia.");
+        }
+        else
+        {
+            for (int i = 0; i < lista.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {lista[i]}");
+            }
+        }
+    }
+
+    static void AdicionarItem(List<string> lista, string item)
+    {
+        lista.Add(item);
+        Console.WriteLine($"{item} adicionado à lista.");
+    }
+
+    static void RemoverItem(List<string> lista, string item)
+    {
+        if (lista.Contains(item))
+        {
+            lista.Remove(item);
+            Console.WriteLine($"{item} removido da lista.");
+        }
+        else
+        {
+            Console.WriteLine($"{item} não encontrado na lista.");
+        }
+    }
+
+    static void LimparLista(List<string> lista)
+    {
+        lista.Clear();
+        Console.WriteLine("Lista de compras limpa.");
     }
 }
