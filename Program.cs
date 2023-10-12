159332ff95b1a6d225bd2449f36748321e1eb2f4
riicardods <riicardoljn@icloud.com>
2023-10-11 22:05:15 -0300

    Parametros aula

--- Program.cs
+++ Program.cs
@@ -5,99 +5,19 @@ class Program
 {
     static void Main()
     {
-        List<string> listaDeCompras = new List<string>();
-
-
-        while (true)
-
-        {
-            Console.WriteLine("=== Lista de Compras ===");
-            ExibirListaDeCompras(listaDeCompras);
-
-            Console.WriteLine("\nOpções:");
-            Console.WriteLine("1. Adicionar item");
-            Console.WriteLine("2. Remover item");
-            Console.WriteLine("3. Limpar lista");
-            Console.WriteLine("4. Sair");
-
-            Console.Write("Escolha uma opção: ");
-            string escolha = Console.ReadLine();
-
-
-
-            switch (escolha)
-            {
-                case "1":
-                    Console.Write("Digite o item a ser adicionado: ");
-                    string item = Console.ReadLine();
-                    AdicionarItem(listaDeCompras, item);
-                    break;
-
-                case "2":
-                    Console.Write("Digite o item a ser removido: ");
-                    string itemRemover = Console.ReadLine();
-                    RemoverItem(listaDeCompras, itemRemover);
-                    break;
-
-                case "3":
-                    LimparLista(listaDeCompras);
-                    break;
-
-                case "4":
-                    Console.WriteLine("Saindo do programa. Até mais!");
-                    return;
-
-                default:
-                    Console.WriteLine("Opção inválida. Tente novamente.");
-                    break;
-            }
-
-            Console.Clear();
-        }
+        soma(80,90);
     }
 
 
-    static void ExibirListaDeCompras(List<string> lista)
+
+    static void soma(int n1, int n2) 
     {
-        if (lista.Count == 0)
-        {
-            Console.WriteLine("A lista de compras está vazia.");
-        }
-        else
-        {
-            for (int i = 0; i < lista.Count; i++)
-            {
-                Console.WriteLine($"{i + 1}. {lista[i]}");
-            }
-        }
-    }
-
-
-    static void AdicionarItem(List<string> lista, string item)
-    {
-        lista.Add(item);
-        Console.WriteLine($"{item} adicionado à lista.");
-    }
-
-
-    static void RemoverItem(List<string> lista, string item)
-    {
-        if (lista.Contains(item))
-        {
-            lista.Remove(item);
-            Console.WriteLine($"{item} removido da lista.");
-        }
-        else
-        {
-            Console.WriteLine($"{item} não encontrado na lista.");
-        }
-    }
+        int resultado = n1 + n2;
+        Console.WriteLine($"a soma de {n1} + {n2} é: {resultado}");
+    }  
 
 
 
-    static void LimparLista(List<string> lista)
-    {
-        lista.Clear();
-        Console.WriteLine("Lista de compras limpa.");
-    }
+
+
 }
